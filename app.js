08dec24439b93caeb46971d1181a34a515502c09
zaytsev <andrey3aytsev@yandex.ru>
2021-03-30 00:50:17 +0300

    add endpoints for words

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ dotenv.config();
 
 const app = express();
 
+// Necessary for parsing application/json
+app.use(express.json());
+
 const {
   CONNECTION_HOST,
   CONNECTION_PORT,
@@ -27,7 +30,46 @@ app.get('/', (req, res) => {
 });
 
 app.get('/words', (req, res) => {
-  connection.query('select * from `words`;', (error, data) => {
+  connection.query('SELECT * FROM words;', (error, data) => {
+    if (error) console.log(error);
+    res.json(data);
+  });
+});
+
+app.post('/words', (req, res) => {
+  const { origin, translation } = req.body;
+  const sqlRequest = 'INSERT INTO words (origin, translation) VALUES (?, ?)';
+
+  connection.query(sqlRequest, [origin, translation], (error, data) => {
+    if (error) console.log(error);
+    res.json(data);
+  });
+});
+
+app.get('/words/:id', (req, res) => {
+  const id = req.params?.id;
+
+  connection.query('SELECT * FROM words WHERE id = ?', [id], (error, data) => {
+    if (error) console.log(error);
+    res.json(data[0]);
+  });
+});
+
+app.put('/words/:id', (req, res) => {
+  const id = req.params?.id;
+  const { origin, translation } = req.body;
+  const sqlRequest = 'UPDATE words SET origin = ?, translation = ? WHERE id = ?';
+
+  connection.query(sqlRequest, [origin, translation, id], (error, data) => {
+    if (error) console.log(error);
+    res.json(data);
+  });
+});
+
+app.delete('/words/:id', (req, res) => {
+  const id = req.params?.id;
+
+  connection.query('DELETE FROM words WHERE id = ?', [id], (error, data) => {
     if (error) console.log(error);
     res.json(data);
   });
